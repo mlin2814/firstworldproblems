9d369ea0f49b20e01acc7838dc4f448f716491e3
Matthew Lin <Matthew Lin>
2017-03-26 15:06:31 -0400

    Adjusting connection to prepare for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 //Package needed
 var mysql = require('mysql');
 
-//Connection details
-var connection = mysql.createConnection({
+//Connection details for Heroku and local deployment
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     port: 3306,
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'problems_db'
-});
+    });
+};
 
 //Console log of failure/success of connection
 connection.connect(function(err) {
